2b3c6dbb717242fab4b77ae87a39f5de4647e2fa
ba1chx4 <winchestersdc79@gmail.com>
2026-03-13 13:58:31 +0600

    Update notification_service.dart

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,8 +1,7 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
-import '../models/task.dart';
-
+import 'package:productivity_app/models/task.dart';
 class NotificationService {
   static final FlutterLocalNotificationsPlugin _plugin =
       FlutterLocalNotificationsPlugin();
