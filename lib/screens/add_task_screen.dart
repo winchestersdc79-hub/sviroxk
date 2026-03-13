99ef53a0fab5d479e312e2a8ad343c587adab036
ba1chx4 <winchestersdc79@gmail.com>
2026-03-13 14:00:41 +0600

    Update add_task_screen.dart

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../models/task.dart';
+import 'package:productivity_app/models/task.dart';
 
 class AddTaskScreen extends StatefulWidget {
   const AddTaskScreen({super.key});
